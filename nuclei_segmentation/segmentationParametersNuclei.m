14310bf92c53a97c0e9dd1d2f578e2964287e37b
Jose L. Cadavid <joseluis.cadavidcardenas@mail.utoronto.ca>
2021-08-08 11:12:09 -0400

    Clean code

--- nuclei_segmentation/segmentationParametersNuclei.m
+++ nuclei_segmentation/segmentationParametersNuclei.m
@@ -1,7 +1,7 @@
-function p = segmentationParametersNuclei()
+function PARAM = segmentationParametersNuclei()
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% This function returns a structure p with parameters for nuclei
+% This function returns a structure PARAM with parameters for nuclei
 % segmentation. High-level parameter tuning should be done with this file
 %
 % Author: Jose L. Cadavid, University of Toronto, 2021
@@ -12,25 +12,25 @@ function p = segmentationParametersNuclei()
 % All volumes in cubic microns
 
 % Minimum volume allowable for blob
-p.vMin = 245;
+PARAM.vMin = 245;
 % Median volume of single nuclei
-p.vMedian = 528;
+PARAM.vMedian = 528;
 % Rough maximum volume allowable per single nuclei
-p.vMax = 1228;
+PARAM.vMax = 1228;
 % Number of white neighbours below which a voxel is considered a boundary
 % voxel (20-24 is good)
-p.nNeigh=20;
+PARAM.nNeigh=20;
 
 %% Penalties for the objective function to determine the optimal number of ellipsoids
 
 % Penalty for image mismatch
-p.pM = 0.1;
+PARAM.pM = 0.1;
 % Penalty for ellipsoids being outside of size range
-p.pV = 0.1;
+PARAM.pV = 0.1;
 % Penalty for ellipsoids overlapping
-p.pO = 0.05;
+PARAM.pO = 0.05;
 % Penalty for aspect ratio (prevent super ellongated ellipsoids)
-p.AR = 0.02;
+PARAM.AR = 0.02;
 
 % Parameters calculating penalties based on logistic functions in the form
 % [k xm]. xm is the value at which the penalty is 50% and xmin is the value
@@ -40,18 +40,18 @@ p.AR = 0.02;
 % For overlap
 xm = 20;
 xmin = 10;
-p.logOver = [-log(1/0.05-1)/(xmin-xm), xm];
+PARAM.logOver = [-log(1/0.05-1)/(xmin-xm), xm];
 % For ellipsoid volume: Lower range - defined in proportion to the minimum
 % nuclear size
-xm = 0.9*p.vMin;
-xmin = 0.7*p.vMin;
-p.logVol(1,:) = [-log(1/0.05-1)/(xmin-xm), xm];
+xm = 0.9*PARAM.vMin;
+xmin = 0.7*PARAM.vMin;
+PARAM.logVol(1,:) = [-log(1/0.05-1)/(xmin-xm), xm];
 % For ellipsoid volume: Upper range - defined in proportion to max nuclear
 % size
-xm = 1.6*p.vMax;
-xmin = 1.0*p.vMax;
-p.logVol(2,:) = [-log(1/0.05-1)/(xmin-xm), xm];
+xm = 1.6*PARAM.vMax;
+xmin = 1.0*PARAM.vMax;
+PARAM.logVol(2,:) = [-log(1/0.05-1)/(xmin-xm), xm];
 % For aspect ratios
 xm = 3;
 xmin = 2;
-p.logAR = [-log(1/0.05-1)/(xmin-xm), xm];
+PARAM.logAR = [-log(1/0.05-1)/(xmin-xm), xm];
--- nuclei_segmentation/splitErosion.m
+++ nuclei_segmentation/splitErosion.m
@@ -1,53 +1,65 @@
-function im2=splitErosion(im,S,Sc)
+function imgSplit = splitErosion(img,S)
 
-%%% Erode a blob in im with structuring element S, relabel and dilate EACH
-%%% blob (that would be the difference between this and opening)
+% splitErosion takes a binary image and attempts to break blobs by
+% performing erosion, relabelling, and dilation of new labels (the
+% relabelling makes this different from morphological opening).
+%
+% USAGE:
+%
+%   imgSplit = splitErosion(img,S)
+%
+% INPUTS:
+%
+%   img:    Binary image
+%   S:      Spherical structuring element created with strel()
+%
+% OUTPUTS:
+%
+%   imgSplit:    Label image of split blobs
+%
+% AUTHOR:
+%   Jose L. Cadavid, University of Toronto, 2021
 
-[M,N,P]=size(im);
-%Erode image to see if it is split
-im=imerode(im,S);
-%Relabel new image
-L=bwlabeln(im);
-%Get voxels in each region
-stats=regionprops3(L,'VoxelIdxList');
+% Get image size
+[M,N,P]=size(img);
+%% Erode image 
+img = imerode(img,S);
+% Relabel new image
+imgLab = bwlabeln(img);
+% Get voxels in each labelled blob
+stats = regionprops3(imgLab,'VoxelIdxList');
+nBlobs = height(stats);
 
-%Dilate each new blob if there is more than one. Some holes might have been
-%opened so we perform closing in each blob (with a smaller structuring
-%element, if possible, to preserve boundary infoin blobs)
+%% Dilate each new blob if there is more than one
 
-%Output image with new labels
-im2=zeros(size(im));
+% Output image with new labels
+imgSplit=zeros(size(img));
 
-%Dilate each image (to process faster take bounding box)
-for i=1:height(stats)
-    
+% Dilate each each blob. Make faster by only taking its bounding box
+for i = 1:nBlobs
     %Get bounding box of blob
-    [x,y,z]=ind2sub(size(im),stats.VoxelIdxList{i});
-    %Create extra padding of 4 (should be a function of the structuring
-    %element, but I am using radius 3) because blob will be dilated
-    xmin=max(1,min(x)-4);
-    xmax=min(M,max(x)+4);
-    ymin=max(1,min(y)-4);
-    ymax=min(N,max(y)+4);
-    zmin=max(1,min(z)-4);
-    zmax=min(P,max(z)+4);
+    % Get x y z coordinates of voxels in blob
+    [x,y,z] = ind2sub(size(img), stats.VoxelIdxList{i});
+    % Create extra padding of 4 (should be a function of the structuring
+    % element, but I am using radius 3) because each blob will be dilated
+    xmin = max(1,min(x)-4);
+    xmax = min(M,max(x)+4);
+    ymin = max(1,min(y)-4);
+    ymax = min(N,max(y)+4);
+    zmin = max(1,min(z)-4);
+    zmax = min(P,max(z)+4);
     
-    %Create dummy image that is bounding box of blob
-    imDummy=zeros(size(im));
-    imDummy(stats.VoxelIdxList{i})=1;
-    %Dilate blob
-    imDummy=imdilate(imDummy(xmin:xmax,ymin:ymax,zmin:zmax),S);
+    % Create dummy image that is bounding box of blob
+    imgDummy = zeros(size(img));
+    imgDummy(stats.VoxelIdxList{i}) = 1;
+    % Dilate blob
+    imgDummy = imdilate(imgDummy(xmin:xmax,ymin:ymax,zmin:zmax),S);
     
-    %Close blob
-%     if imEuler3d(imDummy,6)<1
-%         imDummy=imclose(imDummy,Sc);
-%     end
-    %Put this info in the big image
-    %Find coordinates where voxels are non-zero with respect to to bounding box
-    [xp, yp, zp]=ind2sub([xmax-xmin+1,ymax-ymin+1,zmax-zmin+1],find(imDummy(:)==1));
-    %Translate these coordinates to the coordinate system of the box. Add
-    %the new label. Add 1 to these voxels. That way, things that were
-    %already labelled (overlaps) will have a value > 1
-    im2(sub2ind(size(im),xp+xmin-1,yp+ymin-1,zp+zmin-1))=i; %im2(sub2ind(size(im),xp+xmin-1,yp+ymin-1,zp+zmin-1))+
-    
+    % Put this info in the big image
+    % Find coordinates where voxels are non-zero with respect to to bounding box
+    [xp, yp, zp] = ind2sub([xmax-xmin+1,ymax-ymin+1,zmax-zmin+1],find(imgDummy(:)==1));
+    % Translate these coordinates to the coordinate system of the box. Add
+    % the new label. Add 1 to these voxels. That way, things that were
+    % already labelled (overlaps) will have a value > 1
+    imgSplit(sub2ind(size(img),xp+xmin-1,yp+ymin-1,zp+zmin-1)) = i;     
 end
